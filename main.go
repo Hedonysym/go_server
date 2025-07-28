e21621744a6fec41f42d032448400e294fbacb8c
Hedonysym <dallinhad123@gmail.com>
2025-07-28 18:39:21 -0500

    ch1 l5

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+}
