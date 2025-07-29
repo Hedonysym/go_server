7fa41654cb5ab9a101889cc20c5d0cce17dc8d53
Hedonysym <dallinhad123@gmail.com>
2025-07-29 18:33:49 -0500

    ch1 l11

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", readyEndpointHandler)
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Error starting server:", err)
