c1d0238f58f801584d73f8b1ffd4f209d70fb2b8
Hedonysym <dallinhad123@gmail.com>
2025-07-29 20:16:31 -0500

    ch 2 l2

--- main.go
+++ main.go
@@ -3,17 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverhits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverhits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+
 	mux.HandleFunc("/healthz", readyEndpointHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.metricsResetHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("Error starting server:", err)
---
+++ metrics_handler.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (apiCfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "plain/text; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := apiCfg.fileserverhits.Load()
+	response := fmt.Sprintf(`Hits: %d`, hits)
+	_, err := w.Write([]byte(response))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func (apiCfg *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request) {
+	apiCfg.fileserverhits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("Hits reset to 0"))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
