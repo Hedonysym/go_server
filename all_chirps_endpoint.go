65082d8be8a3e140ca2c2a93b8607a0dab029df7
Hedonysym <dallinhad123@gmail.com>
2025-08-12 17:20:44 -0500

    ch9 l4

--- all_chirps_endpoint.go
+++ all_chirps_endpoint.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,6 +15,7 @@ func (cfg *apiConfig) allChirpsEndpoint(w http.ResponseWriter, r *http.Request) 
 	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
 	defer cancel()
 	authId := r.URL.Query().Get("author_id")
+	ifsort := r.URL.Query().Get("sort")
 	if authId != "" {
 		id, err := uuid.Parse(authId)
 		if err != nil {
@@ -28,6 +30,9 @@ func (cfg *apiConfig) allChirpsEndpoint(w http.ResponseWriter, r *http.Request) 
 		for _, chirp := range chirps {
 			newchirps = append(newchirps, chirpReformatter(chirp))
 		}
+		if ifsort == "desc" {
+			sort.Slice(newchirps, func(i, j int) bool { return newchirps[j].Created_at.Before(newchirps[i].Created_at) })
+		}
 		respondWithJSON(w, 200, newchirps)
 		return
 
@@ -40,6 +45,9 @@ func (cfg *apiConfig) allChirpsEndpoint(w http.ResponseWriter, r *http.Request) 
 		newchirps := []Chirp{}
 		for _, chirp := range chirps {
 			newchirps = append(newchirps, chirpReformatter(chirp))
+		}
+		if ifsort == "desc" {
+			sort.Slice(newchirps, func(i, j int) bool { return newchirps[j].Created_at.Before(newchirps[i].Created_at) })
 		}
 		respondWithJSON(w, 200, newchirps)
 	}
